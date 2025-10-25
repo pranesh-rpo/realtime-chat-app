f28f4bf3114b94d2ce981d836a50a74b8a293ebe
Pranesh <praneshrajavel@yahoo.com>
2025-10-25 17:09:46 +0530

    Use in-memory SQLite database for production deployment

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./chat.db');
+const path = require('path');
+const dbPath = process.env.NODE_ENV === 'production' ? ':memory:' : './chat.db';
+const db = new sqlite3.Database(dbPath);
 
 // Create tables if they don't exist
 db.serialize(() => {
